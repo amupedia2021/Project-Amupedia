ed9f5d9e1080db66723ae92cad2baabc49bb5f09
Haider Ali <malikhaider0567@gmail.com>
2021-12-31 21:14:05 +0530

    Started About Page Building

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withImages = require("next-images");
 module.exports = withImages();
 module.exports = {
-	reactStrictMode: true,
+  reactStrictMode: true,
 };
 
 const path = require("path");
